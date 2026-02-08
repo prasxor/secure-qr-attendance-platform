a11418822d83fca27d8a1cc368c310fd9c4f097b
Prashant kumar sinha <116288719+prasxor@users.noreply.github.com>
2026-02-08 14:24:58 +0530

    update: Add Flask-SQLAlchemy, Flask-Migrate, PyMySQL; update DB config

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,12 +1,20 @@
 from flask import Flask
-from .config import Config
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from .config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     
     CORS(app, resources={r"/api/*": {"origins": "*"}})
+    
+    db.init_app(app)
+    migrate.init_app(app,db)
     
     @app.route("/health")
     def health_check():
--- backend/app/config.py
+++ backend/app/config.py
@@ -2,3 +2,9 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
+    
+    SQLALCHEMY_DATABASE_URI = (
+        "mysql+pymysql://root:@localhost/secure_attendance_system"
+    )
+    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
